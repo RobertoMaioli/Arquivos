54cf970c27dbc1e440dab58224743e0ed3704f39
RobertoMaioli <roberto@maiolidesign.com>
2016-05-06 11:17:19 -0300

    Remove itens

--- AppMaioliDesign/com.myapp.app/www/app.js
+++
@@ -1,8 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('TextoListCtrl', function ($scope) {
-  $scope.textos = [
-   {'logo': 'Nome do Logo'},
-   {'titulo': 'Painel'}
-  ];
-});
